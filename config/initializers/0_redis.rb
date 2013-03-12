ce931c0ca0ace6cde01a4813c6216d2e4aec1a01
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-03-12 16:37:15 -0700

    updated config

--- config/initializers/0_redis.rb
+++ config/initializers/0_redis.rb
@@ -1,7 +1,7 @@
 # this file defines $redis_config which is required by other initializers
 # so it needs to be loaded first
 
-conf = YAML::load(File.open(File.join(Rails.root, 'config/server/redis.yml')))['production']
+conf = YAML::load(File.open(File.join(Rails.root, 'config/server/redis.yml')))[Rails.env]
 
 $redis_config = if ENV['REDISTOGO_URL'].present?
   { url: ENV['REDISTOGO_URL'] }
