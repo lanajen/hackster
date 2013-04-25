5599c0d73a1298fbb414bae661f5b9f0c0fb8eb8
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-04-24 23:32:28 -0700

    bonsai

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,9 +1,14 @@
-BONSAI_INDEX_NAME = if ENV['BONSAI_INDEX_URL']
-  Tire.configure do
-    url "http://index.bonsai.io"
-  end
-  ENV['BONSAI_INDEX_URL'][/[^\/]+$/]
-else
-  app_name = Rails.application.class.parent_name.underscore.dasherize
-  "#{app_name}-#{Rails.env}"
-end
+ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
+
+#BONSAI_INDEX_NAME = if ENV['BONSAI_URL']
+#  Tire.configure do
+#    url "http://index.bonsai.io"
+#  end
+#  ENV['BONSAI_URL'][/[^\/]+$/]
+#else
+#  app_name = Rails.application.class.parent_name.underscore.dasherize
+#  "#{app_name}-#{Rails.env}"
+#end
+
+app_name = Rails.application.class.parent_name.underscore.dasherize
+BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
