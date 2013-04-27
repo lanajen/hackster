e3e8124c2f8a333e0e47b2dd45642070c290a1cd
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-04-26 18:06:39 -0700

    search count

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     @last_projects = Project.limit(4).order('created_at DESC')
     if current_user
       @custom_projects_query = current_user.interest_tags.pluck(:name).join(' OR ')
-      @custom_projects = SearchRepository.new(query: @custom_projects_query, model: { project: 1 }).search.results
+      @custom_projects = SearchRepository.new(query: @custom_projects_query, model: { project: 1 }, size: 4).search.results if @custom_projects_query
     end
   end
 end
--- app/models/search_repository.rb
+++ app/models/search_repository.rb
@@ -12,19 +12,20 @@ class SearchRepository
     models = params[:model] ? params[:model].keys : nil
     query += ' ' + params[:product_tag].keys.join(' ') if params[:product_tag]
     query += ' ' + params[:tech_tag].keys.join(' ') if params[:tech_tag]
-    results = self.search_models query, params[:page], models
+    results = self.search_models query, params[:page], models, params[:size]
 #    Search.create(user_id: user_id, results_count: results.count, query: query) unless params[:page]
     results
   end
 
   protected
-    def search_models query, page=nil, models=nil
+    def search_models query, page=nil, models=nil, size=nil
       filters = []
       filters << { terms: { model: models } } if models.present?
       Rails.logger.info "Searching for #{query} and model #{models.to_s}"
       Tire.search BONSAI_INDEX_NAME, load: true, page: page, per_page: RESULTS_PER_PAGE do
         query { string query, default_operator: 'AND' } if query.present?
         filter :or, filters if filters.any?
+        size size || RESULTS_PER_PAGE
       end
     end
 end
