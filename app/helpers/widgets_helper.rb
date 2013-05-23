859d8e0a2374edaafeecb2af05003f5e7909f845
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-05-23 11:55:44 -0700

    more design changes

--- app/helpers/widgets_helper.rb
+++ app/helpers/widgets_helper.rb
@@ -2,4 +2,13 @@ module WidgetsHelper
   def widget_form_path project, widget
     @widget.persisted? ? project_widget_path(project, widget) : project_widgets_path(project)
   end
+
+  def widget_span_class widget
+    case widget
+    when PartsWidget
+      'span12'
+    else
+      'span6 offset3'
+    end
+  end
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -2,10 +2,12 @@ class Part < ActiveRecord::Base
   belongs_to :partable, polymorphic: true
   attr_accessible :name, :quantity, :unit_price, :vendor_link, :vendor_name,
     :vendor_sku, :partable_id, :partable_type
+  validates :unit_price, :quantity, presence: true
   after_validation :compute_total_cost
 
   private
     def compute_total_cost
+      return false if unit_price.blank? or quantity.blank?
       self.total_cost = (unit_price * quantity.to_f).round(2)
     end
 end
