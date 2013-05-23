1073551870dd308b86908b7b8a70d0ea5b79f6a2
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-05-23 12:04:23 -0700

    foo

--- db/migrate/20130519024816_add_project_to_widgets.rb
+++ db/migrate/20130519024816_add_project_to_widgets.rb
@@ -1,6 +1,6 @@
 class AddProjectToWidgets < ActiveRecord::Migration
   def change
-    add_column :widgets, :project_id, :integer, null: false#, default: 0
+    add_column :widgets, :project_id, :integer, null: false, default: 0
     add_index :widgets, :project_id
   end
 end
