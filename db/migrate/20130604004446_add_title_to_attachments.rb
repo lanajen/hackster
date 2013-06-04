f404865f7fe490c2dee0f47498543bf3d0d6c496
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-06-03 17:57:27 -0700

    added title for attachements

---
+++ db/migrate/20130604004446_add_title_to_attachments.rb
@@ -0,0 +1,5 @@
+class AddTitleToAttachments < ActiveRecord::Migration
+  def change
+    add_column :attachments, :title, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130531223557) do
+ActiveRecord::Schema.define(:version => 20130604004446) do
 
   create_table "access_group_members", :force => true do |t|
     t.integer  "access_group_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20130531223557) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "caption"
+    t.string   "title"
   end
 
   add_index "attachments", ["attachable_id", "attachable_type", "type"], :name => "index_attachments_on_attachable_id_and_attachable_type_and_type"
