5906c4ac407fa2b9843b78d53f0779d2d5bc5240
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-06-11 23:04:30 -0700

    added last updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,7 @@ class PagesController < ApplicationController
   def home
     @featured_projects = Project.indexable.featured.limit 4
     @last_projects = Project.indexable.order('created_at DESC').limit(4)
+    @last_updated_projects = Project.indexable.order('updated_at DESC').limit(4)
 #    if user_signed_in?
 #      @custom_projects_query = current_user.interest_tags.pluck(:name).join(' OR ')
 #      @custom_projects = @custom_projects_query.present? ? SearchRepository.new(query: @custom_projects_query, model: { project: 1 }, size: 4).search.results : []
