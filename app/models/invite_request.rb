290563d8ec575dbb9a2411d17a5ac2b46f242a03
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-07-25 10:48:41 +0200

    allow for .co.uk type of emails

--- app/models/invite_request.rb
+++ app/models/invite_request.rb
@@ -5,7 +5,7 @@ class InviteRequest < ActiveRecord::Base
 
   validates :email, presence: true
   validates :email, uniqueness: { message: 'has already requested an invite' }
-  validates :email, format: { with: /^\b[a-z0-9._%-\+]+@[a-z0-9.-]+\.[a-z]{2,4}\b$/, message: 'is not a valid email address'}
+  validates :email, format: { with: /^\b[a-z0-9._%-\+]+@[a-z0-9.-]+\.[a-z]{2,4}(\.[a-z]{2,4})?\b$/, message: 'is not a valid email address'}
 
   self.per_page = 20
 
