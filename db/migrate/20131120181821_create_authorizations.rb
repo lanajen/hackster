3e4bcd8522678857bcf00a1aec7a05c2d3a0c4ac
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-11-26 20:07:02 -0800

    fix auth migration

--- db/migrate/20131120181821_create_authorizations.rb
+++ db/migrate/20131120181821_create_authorizations.rb
@@ -1,9 +1,9 @@
 class CreateAuthorizations < ActiveRecord::Migration
   def change
     create_table :authorizations do |t|
-      t.string :uid, null: false
+      t.string :uid, null: false, limit: 100
       t.string :provider, null: false, limit: 50
-      t.integer :user_id, null: false, limit: 100
+      t.integer :user_id, null: false
       t.string :name
       t.string :link
 
