62f6056b52291cd7183b42447a45143e458d7ce5
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-12-23 13:45:33 -0800

    more licenses + fixed it

--- app/models/license.rb
+++ app/models/license.rb
@@ -28,10 +28,10 @@ class License
       name: 'Eclipse Public License (EPL-1.0)',
       abbr: 'EPL-1.0', },
     'http://www.tapr.org/OHL' => {
-      name: 'The TAPR Open Hardware License (TAPR-OHL)',
+      name: 'TAPR Open Hardware License (TAPR-OHL)',
       abbr: 'TAPR-OHL', },
     'http://www.balloonboard.org/docs/Balloon_License_0v2.pdf' => {
-      name: 'The Balloon Open Hardware License (BOHL)',
+      name: 'Balloon Open Hardware License (BOHL)',
       abbr: 'BOHL', },
     'http://www.opencollector.org/hardlicense/hdpl.html' => {
       name: 'Hardware Design Public License Version 0.04 (HDPL)',
@@ -40,11 +40,35 @@ class License
       name: 'CERN Open Hardware Licence (CERN-OHL)',
       abbr: 'CERN-OHL', },
     'http://solderpad.org/licenses/' => {
-      name: 'The Solderpad Hardware License (SHL)',
+      name: 'Solderpad Hardware License (SHL)',
       abbr: 'SHL', },
+    'http://creativecommons.org/licenses/by/4.0' => {
+      name: 'Creative Commons Attribution CC BY (CC BY)',
+      abbr: 'CC BY', },
+    'http://creativecommons.org/licenses/by-sa/4.0' => {
+      name: 'Creative Commons Attribution-ShareAlike CC BY-SA (CC BY-SA)',
+      abbr: 'CC BY-SA', },
+    'http://creativecommons.org/licenses/by-nd/4.0' => {
+      name: 'Creative Commons Attribution-NoDerivs CC BY-ND (CC BY-ND)',
+      abbr: 'CC BY-ND', },
+    'http://creativecommons.org/licenses/by-nc/4.0' => {
+      name: 'Creative Commons Attribution-NonCommercial CC BY-NC (CC BY-NC)',
+      abbr: 'CC BY-NC', },
+    'http://creativecommons.org/licenses/by-nc-sa/4.0' => {
+      name: 'Creative Commons Attribution-NonCommercial-ShareAlike CC BY-NC-SA (CC BY-NC-SA)',
+      abbr: 'CC BY-NC-SA', },
+    'http://creativecommons.org/licenses/by-nc-nd/4.0' => {
+      name: 'Creative Commons Attribution-NonCommercial-NoDerivs CC BY-NC-ND (CC BY-NC-ND)',
+      abbr: 'CC BY-NC-ND', },
   }
 
   attr_reader :url, :name, :abbr
+
+  def self.all
+    LICENSE_TYPES.map do |url, attrs|
+      new url
+    end.sort_by{ |l| l.name }
+  end
 
   def initialize url
     @url = url
