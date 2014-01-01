0a9212eeb16bccb0867a504cdde29dce7bc51f28
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-01-01 23:32:02 +0100

    github widget

--- app/models/widgets/github_widget.rb
+++ app/models/widgets/github_widget.rb
@@ -1,6 +1,6 @@
 class GithubWidget < Widget
   define_attributes [:repo]
-  validates :repo, format: { with: /github\.com\/[0-9a-z_]+\/[0-9a-z_]+\z/,
+  validates :repo, format: { with: /github\.com\/[0-9a-z_\-]+\/[0-9a-z_\-]+\z/,
     message: 'is not a valid Github repository' }, allow_blank: true
 
   def self.model_name
