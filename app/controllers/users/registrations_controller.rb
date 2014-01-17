26b10b5242f58e59f8d38c55bcb41bfb3e9b7896
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-01-17 10:43:47 +0100

    tracking sign up page visit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, only: [:create, :update]
-  # def new
-  #   options = { invitation_code: params[:invitation_code] }
-  #   build_resource(options)
-  #   respond_with self.resource
-  # end
+
+  def new
+    track_event 'Visited sign up page'
+
+    super
+  end
 
   protected
     def after_sign_up_path_for(resource)
