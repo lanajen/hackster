6c60cede0eb5bf77ab5e61e9d1eb6c786630c7cd
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-02-16 14:10:11 -0800

    assignment instructions

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,9 +1,15 @@
 class Assignment < ActiveRecord::Base
   belongs_to :promotion
   has_many :projects
+  has_one :document, as: :attachable, dependent: :destroy
   validates :promotion_id, :name, presence: true
+  attr_accessible :name, :document_id
 
   before_create :generate_id
+
+  def document_id=(val)
+    self.document = Document.find_by_id(val)
+  end
 
   def to_label
     "#{promotion.course.name} @#{promotion.course.university.full_name} > #{promotion.name} > #{name}"
