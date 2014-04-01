f2862be774225e4bd9df0fac49793c13b5ac35f2
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-04-01 13:49:05 -0700

    display for big teams

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,3 +30,21 @@
 // require_tree ./backbone/entities
 // require_tree ./backbone/views
 // require_tree ./backbone/apps
+
+(function($) {
+
+    var oldHide = $.fn.popover.Constructor.prototype.hide;
+
+    $.fn.popover.Constructor.prototype.hide = function() {
+        if (this.options.trigger === "hover" && this.tip().is(":hover")) {
+            var that = this;
+            // try again after what would have been the delay
+            setTimeout(function() {
+                return that.hide.call(that, arguments);
+            }, that.options.delay.hide);
+            return;
+        }
+        oldHide.call(this, arguments);
+    };
+
+})(jQuery);
