7fe5bf5125549165e83a3c02648abc2c041a3690
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-04-01 20:29:24 -0700

    one column with sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,13 @@ module ApplicationHelper
     content_tag(:li, link_to(content, link), class: klass)
   end
 
+  def affix_top project, user=nil
+    affix = 725
+    affix += 53 if project.collection_id.present?
+    affix += 52 if project.private and user and user.can? :edit, @project
+    affix
+  end
+
   def class_for_alert alert, notice
     if alert
       return { class: 'alert-danger' }
