67574855e4e0dffa6ab696ec05835be4756fe098
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-04-17 18:28:56 -0700

    fix comments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.project-show .well').on('keypress', '#new_comment textarea', function(){
+  $('.project-show .well').on('keypress', '#new_comment textarea', function(event){
     if (event.which == 13 && !event.shiftKey) {
       event.preventDefault();
       $(this).parents('form').submit();
@@ -10,16 +10,6 @@ $(document).ready(function(){
   $('.project-show .well').on('keyup keydown', '#new_comment textarea', function(){
     var t=$(this);
     t.height(15).height(t[0].scrollHeight);//where 15 is minimum height of textarea
-  });
-
-  // auto adjust the height of
-  $('.project-show .well').on('focus', '#new_comment textarea', function(){
-    $(this).tooltip('show');
-  });
-
-  // auto adjust the height of
-  $('.project-show .well').on('focusout', '#new_comment textarea', function(){
-    $(this).tooltip('destroy');
   });
 
   $('.issue-content').on('click', '.issue-comment-cancel', function(e){
