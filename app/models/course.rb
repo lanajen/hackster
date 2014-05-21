0171f750a067c61f4863eb3adc4e07215047fe2a
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-05-21 14:51:32 -0700

    create any group from admin + make courses use parent_id and not own table for university assoc

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,11 @@
 class Course < Community
-  has_one :courses_university
-  has_one :university, through: :courses_university
+  # has_one :courses_university
+  # has_one :university, through: :courses_university
+  belongs_to :university, foreign_key: :parent_id
   has_many :promotions, foreign_key: :parent_id
   validate :university_is_selected
+
+  attr_accessible :parent_id
 
   def generate_user_name
     slug = name.gsub(/[^a-zA-Z0-9\-_]/, '-').gsub(/(\-)+$/, '').gsub(/^(\-)+/, '').gsub(/(\-){2,}/, '-').downcase
