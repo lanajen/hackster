bd34119f841623335e1a2d809aa64c36b9a395d2
Matt Nish <matt.nish@gmail.com>
2014-06-02 21:06:27 -0700

    Testing mobile nav, added responsive css for mobile-first design

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -131,6 +131,27 @@
         window.location.hash = e.target.hash.replace("#", "#" + prefix);
     });
 
+    // Set up mobile nav menu/trigger
+    var $outerWrapper = $('#outer-wrapper'),
+        $moNavOverlay = $('#mobile-nav-overlay'),
+        $moNav        = $('#mobile-navigation');
+    $('#nav-mobile-trigger').on('click',function(){
+      $outerWrapper.addClass('mobile-nav-open');
+      $moNavOverlay.show();
+      setTimeout(function(){
+        $moNavOverlay.addClass('showing');
+        $moNav.addClass('showing');
+      },1);
+    });
+    $moNavOverlay.on('click',function(){
+      $moNavOverlay.removeClass('showing');
+      $moNav.removeClass('showing');
+      $outerWrapper.removeClass('mobile-nav-open');
+      setTimeout(function(){
+        $moNavOverlay.hide();
+      },500);
+    });
+
   });
   // affixes .affixable
   var $fixedEl;
