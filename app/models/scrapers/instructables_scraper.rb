36089dcfb76f18abb62b360659919f6ce121d123
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-06-24 19:57:48 -0700

    fix scraper

--- app/models/scrapers/instructables_scraper.rb
+++ app/models/scrapers/instructables_scraper.rb
@@ -17,7 +17,7 @@ class InstructablesScraper
   end
 
   def scrape
-    puts "Scraping instructables.com for #{tech}"
+    puts "[Scraper] Scraping instructables.com for #{tech}"
     offset = 0
     results = { errors: [], successes: 0, skips: 0 }
 
@@ -27,28 +27,32 @@ class InstructablesScraper
       lis = parsed.css('li')
       break if lis.empty?
       lis.each do |li|
-        name = li.at_css('.title').text.strip
-        author = li.at_css('.author').text.gsub(/by/, '').strip
-        link = 'http://www.instructables.com' + li.at_css('.title a')['href']
-        next unless li.at_css('.views')
-        if project = Project.find_by_website(link)
-          puts "Found existing project on instructables.com: #{link}"
-          if project.external
-            project.tech_tags_string += ",#{tech}" unless project.tech_tags_string =~ Regexp.new(tech)
-            project.save
-          end
-          results[:skips] += 1
-        else
-          puts "Scraping project in #{tech}: #{name} at #{link}"
-          image = li.at_css('img')['src'].gsub(/RECTANGLE1/, 'LARGE')
-          project = Project.new name: name, guest_name: author, website: link, one_liner: name, tech_tags_string: tech
-          project.build_cover_image
-          project.cover_image.remote_file_url = image
-          project.external = true
-          if project.save
-            results[:successes] += 1
+        begin
+          next unless li.at_css('.views')
+          name = li.at_css('.title').text.strip
+          author = li.at_css('.author').text.gsub(/by/, '').strip
+          link = 'http://www.instructables.com' + li.at_css('.title a')['href']
+          if project = Project.find_by_website(link)
+            puts "[Scraper] Found existing project on instructables.com: #{link}"
+            if project.external
+              project.tech_tags_string += ",#{tech}" unless project.tech_tags_string =~ Regexp.new(tech)
+              project.save
+            end
+            results[:skips] += 1
           else
-            results[:errors] << link
+            puts "[Scraper] Scraping project in #{tech}: #{name} at #{link}"
+            image = li.at_css('img')['src'].gsub(/RECTANGLE1/, 'LARGE')
+            project = Project.new name: name, guest_name: author, website: link, one_liner: name, tech_tags_string: tech
+            project.build_cover_image
+            project.cover_image.remote_file_url = image
+            project.external = true
+            if project.save
+              results[:successes] += 1
+            else
+              results[:errors] << link
+            end
+          rescue => e
+            puts "Rescue error: #{e.inspect}"
           end
         end
       end
