4a03771333cc7d73c6a564b86ec68043cb066dae
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-07-01 19:03:07 -0700

    move thingiview to assets

---
+++ vendor/assets/javascripts/thingiview.js
@@ -0,0 +1,6 @@
+//= require thingiview/Three
+//= require thingiview/plane
+//= require thingiview/thingiview
+//= require thingiview/binaryReader
+//= require thingiview/stats
+//= require thingiview/thingiloader
