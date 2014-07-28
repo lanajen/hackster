d0125857450cba42463525f85658389c551d50f5
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-07-27 19:23:03 -0700

    fix URI stuff

--- app/models/project.rb
+++ app/models/project.rb
@@ -211,6 +211,12 @@ class Project < ActiveRecord::Base
     self.slug = new_slug
   end
 
+  def buy_link_host
+    URI.parse(buy_link).host.gsub(/^www\./, '')
+  rescue
+    buy_link
+  end
+
   def compute_popularity
     self.popularity_counter = ((respects_count * 2 + impressions_count * 0.1 + followers_count * 2 + comments_count * 5 + featured.to_i * 10) * [[(1.to_f / Math.log10(age)), 10].min, 0.01].max).round(4)
   end
