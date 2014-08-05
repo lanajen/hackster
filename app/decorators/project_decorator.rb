a6805a542defc260213d524471a5948d70080848
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-08-05 15:59:50 -0700

    refactoring decorators

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -1,4 +1,5 @@
 class ProjectDecorator < ApplicationDecorator
+  include MediumEditorDecorator
 
   def cover_image version=:cover
     if model.cover_image and model.cover_image.file_url
@@ -9,46 +10,7 @@ class ProjectDecorator < ApplicationDecorator
   end
 
   def description
-    if model.description.present?
-      parsed = Nokogiri::HTML::DocumentFragment.parse model.description
-
-      parsed.css('.embed-frame').each do |el|
-        type = el['data-type']
-
-        embed = case type
-        when 'url'
-          link = el['data-url']
-          next unless link
-
-          Embed.new url: link, fallback_to_default: true
-        when 'file'
-          file_id = el['data-file-id']
-          next unless file_id
-
-          Embed.new file_id: file_id
-        when 'widget'
-          Embed.new widget_id: el['data-widget-id']
-        else
-          next
-        end
-
-        code = h.render partial: "api/embeds/embed", locals: { embed: embed }
-        next unless code
-
-        if caption = el['data-caption']
-          code = Nokogiri::HTML::DocumentFragment.parse code
-          if figcaption = code.at_css('figcaption')
-            figcaption.content = caption
-            code = code.to_html
-          end
-        end
-
-        el.add_child code if code
-      end
-      parsed.to_html.html_safe
-    else
-      "<p class='paragraph--empty'><br></p>".html_safe
-    end
+    parse_medium model.description
   end
 
   def logo size=nil, use_default=true
