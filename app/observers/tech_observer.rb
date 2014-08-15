1bf41e7efae803aca71484d54480f8f177986e82
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-08-15 11:22:50 -0700

    fix autolink + hide/show code editor + tech cache

--- app/observers/tech_observer.rb
+++ app/observers/tech_observer.rb
@@ -27,7 +27,7 @@ class TechObserver < ActiveRecord::Observer
       Cashier.expire *keys if keys.any?
     end
 
-    if (record.changed & %w(full_name avatar mini_resume slug user_name forums_link documentation_link crowdfunding_link buy_link twitter_link facebook_link linked_in_link blog_link github_link website_link youtube_link google_plus_link)).any?
+    if (record.changed & %w(full_name avatar mini_resume slug user_name forums_link documentation_link crowdfunding_link buy_link twitter_link facebook_link linked_in_link blog_link github_link website_link youtube_link google_plus_link logo)).any?
       Cashier.expire "tech-#{record.id}-sidebar"
     end
   end
