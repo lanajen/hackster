c4f6f5b307bf280ddd76a3ea8c2d79613fd3dd40
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-08-18 13:35:45 -0700

    fix scapr queue'

--- app/workers/scraper_queue.rb
+++ app/workers/scraper_queue.rb
@@ -32,7 +32,8 @@ class ScraperQueue < BaseWorker
     @project.build_team
     @project.team.members.new(user_id: user_id)
     @project.tech_tags_string = tech_tags_string if tech_tags_string.present?
-    raise ScrapeError, "Couldn't save project because #{@project.errors.messages.map{|k,v| "#{k} #{v.to_sentence};" }.to_sentence" unless @project.save
+    messages = @project.errors.messages.map{|k,v| "#{k} #{v.to_sentence};" }
+    raise ScrapeError, "Couldn't save project because #{messages.to_sentence}" unless @project.save
     @project.update_counters
     @message.subject = "#{@project.name} has been imported to your Hackster.io profile"
     @message.body = "<p>Hi</p><p>This is to let you know that <a href='http://#{APP_CONFIG['full_host']}/projects/#{@project.to_param}'>#{@project.name}</a> has been successfully imported from <a href='#{page_url}'>#{page_url}</a>.</p><p>You can update it and make it public at <a href='http://#{APP_CONFIG['full_host']}/projects/#{@project.to_param}'>http://#{APP_CONFIG['full_host']}/projects/#{@project.to_param}</a>.</p><p><b>Please note:</b> our importer is an experimental feature, your project might need some additional editing before it's ready for prime!</p><p>Cheers<br/>The Hackster.io team</p>"
