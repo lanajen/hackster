1c2c40bf6294123b5ab90d46d51325c3f7ca031e
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-09-05 16:02:15 -0700

    dont wait for image processing to show image

--- app/observers/issue_observer.rb
+++ app/observers/issue_observer.rb
@@ -1,7 +1,7 @@
 class IssueObserver < PostObserver
   def after_commit_on_create record
     BaseMailer.enqueue_email 'new_issue_notification',
-      { context_type: 'issue', context_id: record.id }
+      { context_type: 'issue', context_id: record.id } if record.class == Issue
   end
 
   private
--- app/workers/base_worker.rb
+++ app/workers/base_worker.rb
@@ -3,6 +3,7 @@
 class BaseWorker
   # extend HerokuResqueAutoScale if Rails.env == 'production'
   include Sidekiq::Worker
+  sidekiq_options queue: :default, retry: 5
 
   # class << self
     def perform method, *args
