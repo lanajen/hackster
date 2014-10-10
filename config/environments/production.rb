811bd4341e3c3e0e5c28f156099fd9f7b21b776c
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-10-10 16:05:09 -0700

    fix ckeditor?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ HackerIo::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( admin.css email.css jquery.githubRepoWidget.min.js bitbucket-widget.min.css bitbucket-widget.min.js slick.eot slick.svg slick.ttf slick.woff datepicker.js datepicker.css )
   config.assets.precompile += Ckeditor.assets
+  config.assets.precompile += %w(ckeditor/*)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
