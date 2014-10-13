9020c921e911c92db0e37209c1afe6f00c5476ed
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-10-12 18:00:24 -0700

    increase weight of critical bg tasks + validate urls for import

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec unicorn -p $PORT -c ./config/server/unicorn.rb
-worker: bundle exec sidekiq -q critical,7 -q default,5 -q low,3 -c 3
+worker: bundle exec sidekiq -q critical,10 -q default,5 -q low,2 -c 3
--- app/controllers/project_imports_controller.rb
+++ app/controllers/project_imports_controller.rb
@@ -5,12 +5,12 @@ class ProjectImportsController < ApplicationController
   end
 
   def create
-    if params[:urls].present?
+    if urls_valid?(params[:urls])
       ScraperQueue.perform_async 'scrape_projects', params[:urls], params[:user_id].presence || current_user.id, params[:tech_tags_string]
       send_admin_message(true) unless current_user.is? :admin
       redirect_to root_path, notice: "We will start importing your projects soon. You'll receive an email when it's done."
     else
-      flash.now[:alert] = "Please specify at least one page to import."
+      flash.now[:alert] = @errors.join(' ')
       render action: 'new'
     end
   end
@@ -28,5 +28,23 @@ class ProjectImportsController < ApplicationController
       @message.body += "<p>Product tag: #{params[:product_tags_string]}</p>" if params[:product_tags_string].present?
       @message.body += "<p>Thanks!<br><a href='#{url_for(current_user)}'>#{current_user.name}</a></p><p><a href='http://#{APP_CONFIG['full_host']}/projects/imports/new?user_id=#{current_user.id}&urls=#{params[:urls]}'>Start importing</a></p>"
       BaseMailer.enqueue_generic_email(@message)
+    end
+
+    def urls_valid? urls
+      @errors = []
+
+      if urls.present?
+        urls = urls.gsub(/\r\n/, ',').gsub(/\n/, ',').gsub(/[ ]+/, ',').split(',').reject{ |l| l.blank? }
+        urls.each do |url|
+          if url =~ /hackster\.io/
+            @errors << "Looks like you're trying to import a page from Hackster? If you need help doing something please send us your query through the help widget in the bottom right corner of your screen (the question mark) or email us at team@hackster.io."
+            break
+          end
+        end
+      else
+        @errors << "Please specify at least one page to import."
+      end
+
+      @errors.empty?
     end
 end
