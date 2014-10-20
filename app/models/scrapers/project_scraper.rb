4fc888eb061f4f9075a6ef9eae1242f6954eecf8
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-10-19 23:06:05 -0700

    better had scraper

--- app/models/scrapers/project_scraper.rb
+++ app/models/scrapers/project_scraper.rb
@@ -4,12 +4,12 @@ class ProjectScraper
   attr_accessor :content, :page_url
 
   def self.scrape page_url
-    # s = new page_url
+    s = new page_url
 
     # debug snippet
-    url = File.join(Rails.root, 'app/models/scrapers/test.html')
-    s = new 'http://hackaday.io/project/1998-Portable-Chaos-Monkey'
-    s.content = s.read_file url
+    # url = File.join(Rails.root, 'app/models/scrapers/test.html')
+    # s = new 'http://hackaday.io/project/1998-Portable-Chaos-Monkey'
+    # s.content = s.read_file url
     # end debug snippet
 
     project = s.document.to_project
