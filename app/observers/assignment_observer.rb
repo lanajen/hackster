31ea69aceb4c3aa0a1f29f7b7e0ff353fbba5c04
Benjamin Larralde <benjamin.larralde@gmail.com>
2014-12-03 21:58:42 -0800

    fix assignments

--- app/observers/assignment_observer.rb
+++ app/observers/assignment_observer.rb
@@ -1,15 +1,11 @@
 class AssignmentObserver < ActiveRecord::Observer
   def after_update record
     if record.private_grades_changed? and !record.private_grades
-      record.projects.each do |project|
-        project.locked = false
-        project.save
-      end
+      record.projects.each{ |p| p.update_attribute :locked, false }
 
       record.grades.each do |grade|
         BaseMailer.enqueue_email 'grade_notification',
           { context_type: 'grade', context_id: grade.id }
-        record.projects.each{ |p| p.update_attribute :locked, false }
       end
     end
   end
