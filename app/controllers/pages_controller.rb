76e74b3acb09d864bebd8ac015f978a466d73f46
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-02-06 10:40:17 -0800

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -37,7 +37,7 @@ class PagesController < ApplicationController
     # @active_projects = Project.last_updated.limit 4
     # @featured_projects = Project.featured.limit 4
     # @wip_projects = Project.wip.limit 4
-    @platforms = Platform.where(user_name: %w(spark delorean metawear tinyduino intel-edison wunderbar)).for_thumb_display.order(:full_name)
+    @platforms = Platform.where(user_name: %w(spark delorean metawear tinycircuits intel-edison wunderbar)).for_thumb_display.order(:full_name)
 
     # render (user_signed_in? ? 'home_member' : 'home_visitor')
     render 'home_visitor'
