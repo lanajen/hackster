159ffb10ede2f9b7d359366dddb2bf3f4650b43b
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-02-25 18:16:18 -0800

    hhw

--- app/models/attachments/cover_image.rb
+++ app/models/attachments/cover_image.rb
@@ -1,2 +1,10 @@
-class CoverImage < Image
+class CoverImage < Attachment
+  mount_uploader :file, CoverImageUploader
+
+  before_save :truncate_title
+
+  private
+    def truncate_title
+      self.title = title.truncate 255 if title
+    end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,53 +1,17 @@
 # encoding: utf-8
 
-class ImageUploader < BaseUploader
-  EXTENSION_WHITE_LIST = %w(gif png jpg jpeg ico bmp)
+class ImageUploader < BaseImageUploader
 
-  process :fix_exif_rotation
-
-  version :lightbox, unless: :is_cover? do
+  version :lightbox do
     process resize_to_limit: [1280, 960]
   end
 
-  version :headline, unless: :is_cover? do
+  version :headline do
     process resize_to_limit: [580, 435]
   end
 
-  version :thumb, from_version: :headline, unless: :is_cover? do
+  version :thumb, from_version: :headline do
     process resize_to_limit: [200, 150]
   end
 
-  version :cover, if: :is_cover? do
-    process resize_to_fill: [1600, 1200]
-    # process resize_to_fit: [1600, 1200]
-    # process resize_and_pad: [1600, 1200, '#ffffff']
-  end
-
-  version :cover_thumb, from_version: :cover, if: :is_cover? do
-    process resize_to_fill: [400, 300]
-  end
-
-  version :cover_mini_thumb, if: :is_cover? do
-    process resize_to_fill: [60, 60]
-  end
-
-  def extension_white_list
-    EXTENSION_WHITE_LIST unless model.skip_file_check?# or model.skip_extension_check
-  end
-
-  def needs_processing?
-    true
-  end
-
-  protected
-    def fix_exif_rotation #this is my attempted solution
-      manipulate! do |img|
-        img.tap(&:auto_orient)
-      end
-    rescue
-    end
-
-    def is_cover? picture
-      model.class == CoverImage
-    end
 end
