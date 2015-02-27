ed54d7a4fc3de7857432a2f90bbf298b66f4024e
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-02-26 20:08:40 -0800

    add day to event schedule

--- app/models/schedule_item.rb
+++ app/models/schedule_item.rb
@@ -1,9 +1,10 @@
 class ScheduleItem < Tableless
   belongs_to :event
-  attr_accessible :parent_id, :position, :time, :description
+  attr_accessible :parent_id, :position, :time, :description, :day
 
   column :position, :integer
   column :time, :string
+  column :day, :string
   column :description, :text
   column :parent_id, :integer
 end
