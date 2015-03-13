d6ed3c095897c01eaad1792bf176c5e2c80d91df
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-03-13 11:22:52 -0700

    tweaks for dev site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -420,7 +420,7 @@ class ApplicationController < ActionController::Base
         logger.error ""
         clean_backtrace.each { |line| logger.error "Backtrace: " + line }
         logger.error ""
-        BaseMailer.enqueue_email 'error_notification', { context_type: :log_line, context_id: log_line.id }
+        BaseMailer.enqueue_email 'error_notification', { context_type: :log_line, context_id: log_line.id } if Rails.env == 'production'
       rescue
       end
       @error = exception
