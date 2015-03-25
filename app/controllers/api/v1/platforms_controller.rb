5c64a78121bf0a72126ccc21f8fe911ad1b841f2
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-03-25 16:29:22 -0700

    api

--- app/controllers/api/v1/platforms_controller.rb
+++ app/controllers/api/v1/platforms_controller.rb
@@ -18,10 +18,15 @@ class Api::V1::PlatformsController < Api::V1::BaseController
       sort = params[:sort] ||= 'magic'
       @by = params[:by] || 'all'
 
-      @projects = @platform.project_collections.includes(:project).visible.order('project_collections.workflow_state DESC').merge(Project.indexable_and_external.for_thumb_display_in_collection)
-      if sort and sort.in? Project::SORTING.keys
-        @projects = @projects.merge(Project.send(Project::SORTING[sort]))
+      sort = if params[:sort] and params[:sort].in? Project::SORTING.keys
+        params[:sort]
+      else
+        params[:sort] = @platform.project_sorting.presence || 'magic'
       end
+      @by = params[:by] || 'all'
+
+      @projects = @platform.project_collections.includes(:project).visible.order('project_collections.workflow_state DESC').merge(Project.for_thumb_display_in_collection)
+      @projects = @projects.merge(Project.send(Project::SORTING[sort]))
 
       if @by and @by.in? Project::FILTERS.keys
         @projects = if @by == 'featured'
