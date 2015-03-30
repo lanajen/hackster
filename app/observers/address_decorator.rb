3b5299ab21b1057bb6a742bdf71a217aba40f50f
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-03-30 08:51:34 -0700

    fix content_tag in controllers

--- app/observers/address_decorator.rb
+++ app/observers/address_decorator.rb
@@ -1,5 +1,5 @@
 class AddressDecorator < ApplicationDecorator
   def full
-    model.completed? ? full_name + content_tag(:br) + address_line1 + content_tag(:br) + address_line2 + content_tag(:br) + "#{city}, #{state} #{zip}" + content_tag(:br) + country + content_tag(:br) + "Phone: #{phone}" : 'Not entered yet'
+    model.completed? ? full_name + h.content_tag(:br) + address_line1 + h.content_tag(:br) + address_line2 + h.content_tag(:br) + "#{city}, #{state} #{zip}" + h.content_tag(:br) + country + h.content_tag(:br) + "Phone: #{phone}" : 'Not entered yet'
   end
 end
