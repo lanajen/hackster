b51ead2040f813e51fbf349d05bb34eb096805da
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-04-09 15:47:49 -0700

    fix fix mailer

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -9,17 +9,17 @@ class BaseMailer < ActionMailer::Base
     send action, type, opts
   end
 
-  def enqueue_email type, options
+  def self.enqueue_email type, options
     self.message.perform_deliveries = false
     MailerQueue.perform_async 'message_with_context', type, options[:context_type], options[:context_id]
   end
 
-  def enqueue_devise_email type, options, devise_opts
+  def self.enqueue_devise_email type, options, devise_opts
     self.message.perform_deliveries = false
     MailerQueue.perform_async 'devise_message', type, options[:context_type], options[:context_id], devise_opts
   end
 
-  def enqueue_generic_email message
+  def self.enqueue_generic_email message
     self.message.perform_deliveries = false
     MailerQueue.perform_async 'generic_message', message.name, message.from_email, message.to_email, message.subject, message.body, message.message_type, message.recipients
   end
