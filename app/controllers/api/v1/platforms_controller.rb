9e67a7f54aed2eaa73497e0e07af170d93e1cb6c
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-04-13 12:34:33 -0700

    fix 4 bugs

--- app/controllers/api/v1/platforms_controller.rb
+++ app/controllers/api/v1/platforms_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PlatformsController < Api::V1::BaseController
+  include PlatformHelper
   # before_filter :public_api_methods, only: [:index, :show]
   before_filter :load_platform, only: [:show]
   before_filter :load_projects, only: [:show]
@@ -12,32 +13,6 @@ class Api::V1::PlatformsController < Api::V1::BaseController
   end
 
   private
-    def load_projects
-      per_page = begin; [Integer(params[:per_page]), Project.per_page].min; rescue; Project.per_page end;  # catches both no and invalid params
-
-      sort = params[:sort] ||= 'magic'
-      @by = params[:by] || 'all'
-
-      sort = if params[:sort] and params[:sort].in? Project::SORTING.keys
-        params[:sort]
-      else
-        params[:sort] = @platform.project_sorting.presence || 'magic'
-      end
-      @by = params[:by] || 'all'
-
-      @projects = @platform.project_collections.includes(:project).visible.order('project_collections.workflow_state DESC').merge(Project.for_thumb_display_in_collection)
-      @projects = @projects.merge(Project.send(Project::SORTING[sort]))
-
-      if @by and @by.in? Project::FILTERS.keys
-        @projects = if @by == 'featured'
-          @projects.featured
-        else
-          @projects.merge(Project.send(Project::FILTERS[@by]))
-        end
-      end
-
-      @projects = @projects.paginate(page: safe_page_params, per_page: per_page)
-    end
 
     def load_platform
       @platform = Platform.find_by_user_name! params[:user_name]
