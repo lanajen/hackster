014b3f07ba6f4d4f56dcd149d2ee0799d62bc7ad
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-04-25 23:42:52 -0700

    conf

--- config/environments/dev.rb
+++ config/environments/dev.rb
@@ -85,6 +85,6 @@ HackerIo::Application.configure do
 
   config.cashier.adapter = :cache_store
   if ENV["MEMCACHEDCLOUD_SERVERS"]
-    config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"], :pool_size => 5 }
+    config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"], compress: true }
   end
 end
