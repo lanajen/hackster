5220d0afeeea205003975498227ee587c86e039e
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-04-26 18:53:14 -0700

    cache warming

--- app/workers/cache_worker.rb
+++ app/workers/cache_worker.rb
@@ -1,9 +1,13 @@
 class CacheWorker < BaseWorker
   def turn_up_the_heat
+    app = ActionDispatch::Integration::Session.new(Rails.application)
     app.host! APP_CONFIG['full_host']
-    Project.indexable.each do |project|
+    app.default_url_options = { host: APP_CONFIG['full_host'] }
+    default_url_options = { host: APP_CONFIG['full_host'] }
+
+    Project.indexable.most_popular.each do |project|
       app.get '/' + project.uri unless Rails.cache.exist?(project)
     end
-    app.get '/' unless Rails.cache.exist?('home-visitor')
+    app.get root_path unless Rails.cache.exist?('home-visitor')
   end
 end
