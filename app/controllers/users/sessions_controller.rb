969a8e236b59419c0e7503f655bfa32abcc1bd8c
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-04-30 12:23:31 -0700

    customize sign in/up messages

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -17,7 +17,12 @@ class Users::SessionsController < Devise::SessionsController
       end
     end
 
-    super
+    # is there a way to not have to duplicate the following code to change the flash message?
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :"signed_in_#{site_platform == 'hackster' ? 'hackster' : 'other'}") if is_flashing_format?
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
   end
 
   protected
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -121,7 +121,7 @@ class UsersController < ApplicationController
       if @user.projects.any?
         redirect_to @user.projects.first, notice: "Profile info saved! Now you can start working on your project."
       else
-        redirect_to user_return_to, notice: 'Profile info saved!'
+        redirect_to user_return_to, notice: "Profile info saved. Now, <a href='/talk' class='alert-link'>come introduce yourself to the community!</a>"
       end
 
       track_user @user.to_tracker_profile
