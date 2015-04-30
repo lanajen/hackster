d3a00b48c766ffd62e3c67cd90c99f029ab9f288
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-04-30 03:17:08 -0700

    stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,6 @@
 class StatsController < ApplicationController
   skip_after_filter :track_landing_page, only: [:create]
-  protect_from_forgery except: [:create]
+  protect_from_forgery except: [:legacy, :create]
 
   def create
     impressionist_async({ id: params[:id], type: (params[:type]) }, request.referrer, unique: [:session_hash], referrer: params[:referrer], action_name: params[:a], controller_name: params[:c])
