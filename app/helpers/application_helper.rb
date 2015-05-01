979a70f47127fbb4ca26195f445a5201561ccc7d
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-04-30 23:20:17 -0700

    stats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -220,7 +220,7 @@ module ApplicationHelper
         "
         $(function(){
           $.ajax({
-            url: '://#{APP_CONFIG['stats_host']}/stats',
+            url: '#{APP_CONFIG['stats_url']}/stats',
             data: {
               referrer: document.referrer,
               id: '#{model_id}',
