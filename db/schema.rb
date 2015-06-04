c795cd0f0e84a82dce891f491a0555993a49a9b3
Cory Asato <czasato@gmail.com>
2015-06-03 18:56:41 -0700

    raked db

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20150529051031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
   enable_extension "pg_stat_statements"
+  enable_extension "hstore"
 
   create_table "addresses", force: :cascade do |t|
     t.integer "addressable_id"
