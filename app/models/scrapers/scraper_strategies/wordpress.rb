fc7169ed020d3836cae0f85469ffbea97b2e0827
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-06-11 14:39:21 -0700

    fix scrapers

--- app/models/scrapers/scraper_strategies/wordpress.rb
+++ app/models/scrapers/scraper_strategies/wordpress.rb
@@ -32,7 +32,7 @@ module ScraperStrategies
           next unless body and name
           datetime = comment.at_css('time').try(:[], 'datetime')
           created_at = DateTime.parse datetime if datetime
-          c = @project.comments.new body: body, guest_name: name
+          c = @project.comments.new raw_body: body, guest_name: name
           c.created_at = created_at
           c.parent = parent
           c.disable_notification!
