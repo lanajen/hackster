0d584957eebe768e66507ec5b386d92e2dfa53c9
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-06-16 13:33:01 -0700

    fix urls for emails

--- app/models/url_generator.rb
+++ app/models/url_generator.rb
@@ -1,4 +1,9 @@
 class UrlGenerator
   include Rails.application.routes.url_helpers
   include UrlHelper
+
+  private
+    def default_url_options(options = {})
+      { locale: nil }.merge options
+    end
 end
