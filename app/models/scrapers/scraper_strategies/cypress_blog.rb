b76c78607ace776426ffaacbdb7e99134fc2846d
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-06-20 18:56:23 -0700

    cypress

--- app/models/scrapers/scraper_strategies/cypress_blog.rb
+++ app/models/scrapers/scraper_strategies/cypress_blog.rb
@@ -5,11 +5,13 @@ module ScraperStrategies
         @article.css('table').last.remove
         title = @article.at_css('.BlogPostTitle')
         title.parent.next.remove
-        @project.one_liner = @article.at_css('p').text.strip
-        if @project.one_liner =~ /In today.+ project,/
-          @project.one_liner = @project.one_liner.split(/,/, 2)[1].strip.capitalize
+        if p = @article.at_css('p')
+          @project.one_liner = p.text.strip
+          if @project.one_liner =~ /In today.+ project,/
+            @project.one_liner = @project.one_liner.split(/,/, 2)[1].strip.capitalize
+          end
+          @project.one_liner = @project.one_liner.truncate 140
         end
-        @project.one_liner = @project.one_liner.truncate 140
         @project.platform_tags_string = 'Cypress PSoC'
 
         super
