dbd9dd4604476f228a274a5b5a02c57aa6c98f24
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-06-20 18:37:18 -0700

    cypress import

--- app/models/scrapers/scraper_strategies/cypress_blog.rb
+++ app/models/scrapers/scraper_strategies/cypress_blog.rb
@@ -5,6 +5,12 @@ module ScraperStrategies
         @article.css('table').last.remove
         title = @article.at_css('.BlogPostTitle')
         title.parent.next.remove
+        @project.one_liner = @article.at_css('p').text.strip
+        if @project.one_liner =~ /In today.+ project,/
+          @project.one_liner = @project.one_liner.split(/,/, 2)[1].strip.capitalize
+        end
+        @project.one_liner = @project.one_liner.truncate 140
+        @project.platform_tags_string = 'Cypress PSoC'
 
         super
       end
---
+++ script/cypress_import.rb
@@ -0,0 +1,46 @@
+csv_text = "http://www.cypress.com/?rID=107402&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day001_Find_Me
+http://www.cypress.com/?rID=107452&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day002_Heart_Rate_Sensor
+http://www.cypress.com/?rID=107488&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day003_Custom_Profile_CapSense_RGB_LED
+http://www.cypress.com/?rID=107617&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day004_CapSense_Proximity
+http://www.cypress.com/?rID=107718&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day005_Health_Thermometer
+http://www.cypress.com/?rID=107774&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day006_Central_IAS
+http://www.cypress.com/?rID=107849&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day007_Peripheral_and_Broadcaster
+http://www.cypress.com/?rID=107903&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day008_Constant_Broadcaster
+http://www.cypress.com/?rID=108021&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day009_Dynamic_Broadcaster
+http://www.cypress.com/?rID=108146&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day010_Observer
+http://www.cypress.com/?rID=108280&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day011_Central_Observer
+http://www.cypress.com/?rID=108419&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day012_Central_Peripheral
+http://www.cypress.com/?rID=108579&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day013_Client_Server
+http://www.cypress.com/?rID=108615&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day014_Whitelist
+http://www.cypress.com/?rID=108748&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day015_Bonding
+http://www.cypress.com/?rID=108795&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day016_Authentication
+http://www.cypress.com/?rID=108907&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day017_Privacy
+http://www.cypress.com/?rID=109003&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day018_Direct_Test_Mode
+http://www.cypress.com/?rID=109105&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day019_Connection_Parameters
+http://www.cypress.com/?rID=109234&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day020_BLE_UART
+http://www.cypress.com/?rID=109348&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day021_Dynamic_GATT_Service
+http://www.cypress.com/?rID=109464&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day022_Apple_Notification_Center_Service
+http://www.cypress.com/?rID=109554&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day023_SegmentLCD
+http://www.cypress.com/?rID=109650&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day024_Throughput
+http://www.cypress.com/?rID=109702&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day025_BLE_Over_The_Air_Update
+http://www.cypress.com/?rID=109883&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day026_HRM_DataLogger
+http://www.cypress.com/?rID=109945&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day027_BLE_Power_Measurement
+http://www.cypress.com/?rID=110001&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day028_BLE_Proximity
+http://www.cypress.com/?rID=110097&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day029_BLE_SFlash_Write
+http://www.cypress.com/?rID=110262&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day030_Time_Multiplexed_Central
+http://www.cypress.com/?rID=110394&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day031_Digital_Sensor
+http://www.cypress.com/?rID=110414&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day032_OOB_Pairing
+http://www.cypress.com/?rID=110543&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day033_BLE_RTC
+http://www.cypress.com/?rID=110596&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day034_BLE_RGB_Power_LED_Control
+http://www.cypress.com/?rID=110669&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day035_I2C_BLE_Bridge
+http://www.cypress.com/?rID=110713&cache=0,https://github.com/cypresssemiconductorco/PSoC-4-BLE/tree/master/100_Projects_in_100_Days/Day036_I2C_BLE_Bridge_Client"
+
+require 'csv'
+user_id = 7576
+
+csv = CSV.parse(csv_text, headers: false)
+csv.each do |row|
+  page_url = row[0]
+  website = row[1]
+  ProjectScraper.scrape_and_save page_url, user_id, model_class: ExternalProject, website: website, private: false, workflow_state: :approved
+end
