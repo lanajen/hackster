88b00740641d2ee9ed9cb307ee023887dadb4338
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-07-02 18:46:15 -0700

    fix tags

--- app/observers/tag_observer.rb
+++ app/observers/tag_observer.rb
@@ -1,12 +1,12 @@
 class TagObserver < ActiveRecord::Observer
-  # def after_create record
-  #   if record.type == 'PlatformTag' and record.taggable_type == 'Project'
-  #     project = record.taggable
-  #     project.platform_tags_string_from_tags
-  #     project.save
-  #     ProjectWorker.perform_async 'update_platforms', project.id
-  #   end
-  # end
+  def after_create record
+    if record.type == 'PlatformTag' and record.taggable_type == 'Project'
+      project = record.taggable
+      project.platform_tags_string_from_tags
+      project.save
+      ProjectWorker.perform_async 'update_platforms', project.id
+    end
+  end
   # def after_create record
   #   update_counters record
   # end
