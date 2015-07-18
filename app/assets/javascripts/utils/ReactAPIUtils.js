105c99a8ebf9d68358c2c3df79a5c9bdc97cd045
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-07-17 17:00:15 -0700

    added flag button to comments and thoughts

--- app/assets/javascripts/utils/ReactAPIUtils.js
+++ app/assets/javascripts/utils/ReactAPIUtils.js
@@ -43,6 +43,18 @@ module.exports = {
           err ? reject(err) : resolve(res);
         });
     });
+  },
+
+  flagContent(csrfToken, flaggableType, flaggableId, userId) {
+    return new Promise((resolve, reject) => {
+      request
+        .post('/api/v1/flags')
+        .set('X-CSRF-Token', csrfToken)
+        .send({flag: { flaggable_type: flaggableType,  flaggable_id: flaggableId,  user_id: userId} })
+        .end(function(err, res) {
+          err ? reject(err) : resolve(res);
+        });
+    });
   }
 
 };
