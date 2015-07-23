b22b51b01ca5d6f1e7dbe4d49d82a7b0384faf97
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-07-22 19:32:49 -0700

    dont need addons

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,7 +1,7 @@
 //= require_self
 //= require react_ujs
 
-global.React = require('react/addons');
+global.React = require('react');
 global.FollowButton = require('./follow_button/app');
 global.NotificationDropdown = require('./notification_dropdown/app');
 global.ListsDropdown = require('./lists_dropdown/app');
