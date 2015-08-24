25fd6dc011db78001d528205a048a3350ef8d03d
Cory Asato <czasato@gmail.com>
2015-08-24 11:20:41 -0700

    Working in Node

--- app/assets/javascripts/editor/components/Paragraph.js
+++ app/assets/javascripts/editor/components/Paragraph.js
@@ -61,11 +61,11 @@ const Paragraph = React.createClass({
     // console.log(e, html);
     // this.props.onBlur(html);
 
-    // if(this.state.editable === true) {
-    //   this.setState({
-    //     editable: false
-    //   });
-    // }
+    if(this.state.editable === true) {
+      this.setState({
+        editable: false
+      });
+    }
   },
 
   onEnterKey() {
@@ -86,9 +86,13 @@ const Paragraph = React.createClass({
     }
   },
 
+  onBlockItemChange(e) {
+    console.log('SELECTED');
+  },
+
   render() {
     let content = this.state.editable ? (<ContentEditable key={createRandomNumber()} ref={this.props.refLink} refLink={createRandomNumber()} className="no-outline-focus" html={this.state.html} data-index-pos={this.props.indexPos} cursorPos={this.state.cursorPos} onBlur={this.onBlur} onChange={this.onContentEditableChange} onEnterKey={this.onEnterKey} onCursorPos={this.onCursorPos}/>) : 
-                                        (<p key={createRandomNumber()} style={{margin: 0}} ref={this.props.refLink} template={'<p>hi</p>'} data-index-pos={this.props.indexPos} onClick={this.onClick}>{this.props.content} {this.props.children}</p>); 
+                                        (<p key={createRandomNumber()} style={{margin: 0}} ref={this.props.refLink} template={'<p>hi</p>'} data-index-pos={this.props.indexPos} onChange={this.onBlockItemChange} onClick={this.onClick}>{this.props.content} {this.props.children}</p>); 
     return (
       <div style={{margin: 0}} onMouseDown={this.onMouseDown}>
         {content}
