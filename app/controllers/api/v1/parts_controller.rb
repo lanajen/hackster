48f5a4c90284b57a7b7e07fde13b2b78e2da2fba
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-08-31 17:42:43 -0700

    changes to event page

--- app/controllers/api/v1/parts_controller.rb
+++ app/controllers/api/v1/parts_controller.rb
@@ -8,12 +8,17 @@ class Api::V1::PartsController < Api::V1::BaseController
     end
 
     term = params[:type].gsub(/Part/, '').downcase
+    term = {
+      'hardware' => 'component',
+      'software' => 'app',
+      'tool' => 'tool',
+    }[term]
 
     if parts.any?
       parts_json = parts.map{|p| { id: p.id, text: p.full_name } }
       parts_json << { id: '-1', text: "Can't find the right one? <a href='#' class='new-part-modal-toggle btn btn-sm btn-success' data-target= '#new-#{term}-modal' data-toggle='modal' data-value='#{params[:q]}'>Create a new #{term}</a>".html_safe, disabled: true }
     else
-      parts_json = [{ id: '-1', text: "No results for #{params[:q]}. <a href='#' class='new-part-modal-toggle btn btn-sm btn-success' data-target= '#new-#{term}-modal' data-toggle='modal' data-value='#{params[:q]}'>Create a new #{term}</a>".html_safe, disabled: true }]
+      parts_json = [{ id: '-1', text: "No results for \"#{params[:q]}\". <a href='#' class='new-part-modal-toggle btn btn-sm btn-success' data-target= '#new-#{term}-modal' data-toggle='modal' data-value='#{params[:q]}'>Create a new #{term}</a>".html_safe, disabled: true }]
     end
 
     render json: parts_json, root: false
