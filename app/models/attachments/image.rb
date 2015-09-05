b8bf69ad0b41823c02b2da212afc16bc33c96d09
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-09-04 21:20:08 -0700

    optimizing image sizes so we dont spend as much

--- app/models/attachments/image.rb
+++ app/models/attachments/image.rb
@@ -1,8 +1,9 @@
 class Image < BaseImage
   VERSIONS = {
-    lightbox: { w: 1280, h: 960, fit: :clip },
-    headline: { w: 580, h: 435, fit: :clip },
-    thumb: { w: 200, h: 150, fit: :clip },
+    lightbox: { w: 1280, h: 960, fit: :max },
+    headline: { w: 580, h: 435, fit: :max },
+    medium: { w: 400, h: 300, fit: :max },
+    thumb: { w: 200, h: 150, fit: :max },
   }
   mount_uploader :file, ImageUploader
 end
