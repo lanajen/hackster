84e7639ac1082246303da8e22cb9ef7116d1b7e1
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-09-05 16:43:24 -0700

    more img optimization

--- app/models/attachments/cover_image.rb
+++ app/models/attachments/cover_image.rb
@@ -6,6 +6,7 @@ class CoverImage < BaseImage
     cover_wide_small: { w: 450, h: 130, fit: :min },
     cover_wide_mini: { w: 300, h: 75, fit: :min },
     large: { w: 900, h: 675, fit: :min },
+    medium: { w: 600, h: 450, fit: :min },
     cover_thumb: { w: 400, h: 300, fit: :min },
     cover_mini_thumb: { w: 60, h: 60, fit: :min },
   }
