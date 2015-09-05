410bb4f29da64ff39ced6f52abda8443ba0a35ba
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-09-05 16:48:59 -0700

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -479,7 +479,6 @@ class User < ActiveRecord::Base
 
   def is_staff? project
     PromotionMember.with_group_roles(%w(ta professor)).where(user_id: id).joins("INNER JOIN groups ON groups.id = members.group_id AND groups.type = 'Promotion'").joins("INNER JOIN assignments ON assignments.promotion_id = groups.id").joins("INNER JOIN project_collections ON project_collections.collectable_id = assignments.id AND project_collections.collectable_type = 'Assignment'").where(project_collections: { project_id: project.id }).any?
-    false
   end
 
   def is_team_member? project, all=true
