45568a621b8b283351b36300d238fb90ab85f59f
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-09-05 19:16:49 -0700

    test

--- config/environments/dev.rb
+++ config/environments/dev.rb
@@ -12,6 +12,7 @@ HackerIo::Application.configure do
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS
+  config.assets.css_compressor = :yui
   config.assets.js_compressor = :uglifier
 
   # Generate digests for assets URLs
