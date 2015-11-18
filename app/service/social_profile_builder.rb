cf318d95c86d5a6bd8e2fb4c04f2098d64bd1c84
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-11-18 10:38:16 -0800

    grab image from arduino profile

--- app/service/social_profile_builder.rb
+++ app/service/social_profile_builder.rb
@@ -73,6 +73,8 @@ class SocialProfileBuilder
         user_name: clean_user_name(info.id),
         email: info.email,
       )
+      image_url = "https://dcw9y8se13llu.cloudfront.net/avatars/#{data.uid}.jpg"
+      build_avatar image_url
     end
 
     def facebook info, data
