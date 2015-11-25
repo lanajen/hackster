e98fa0e121dd2680627b453b5d25e6440063d56d
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-11-25 12:11:43 -0800

    adds support for arduino code highlight

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -462,8 +462,7 @@ $select2target = null;
       cEditor.ace.setValue($(this).find('[data-field-type="raw_code"]').val());
       cEditor.ace.selection.clearSelection();
       var lang = $(this).find('[data-field-type="language"]').val();
-      if (lang == '') lang = 'text';
-      cEditor.ace.getSession().setMode("ace/mode/" + lang);
+      updateEditorMode(cEditor, lang);
     });
 
     $('#code-editor-popup').on('modal:open', function(){
@@ -472,9 +471,22 @@ $select2target = null;
 
     $('#code-editor-popup').on('change', '[data-field-type="language"]', function(e){
       var lang = $(this).val();
-      if (lang == '') lang = 'text';
-      cEditor.ace.getSession().setMode("ace/mode/" + lang);
+      updateEditorMode(cEditor, lang);
     });
+
+    function updateEditorMode(editor, lang) {
+      console.log('lang1', lang);
+      switch (lang) {
+        case '':
+          lang = 'text';
+          break;
+        case 'arduino':
+          lang = 'c_cpp';
+          break;
+      }
+      console.log('lang2', lang);
+      editor.ace.getSession().setMode("ace/mode/" + lang);
+    }
 
     $('#code-editor-popup').on('click', '.upload-code', function(e){
       e.preventDefault();
