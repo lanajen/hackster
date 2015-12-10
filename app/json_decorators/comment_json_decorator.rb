0219a7bcb6ee2b278608252236eeab12182b0222
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-12-10 11:32:14 -0800

    fix comments api with deleted users

--- app/json_decorators/comment_json_decorator.rb
+++ app/json_decorators/comment_json_decorator.rb
@@ -1,8 +1,8 @@
 class CommentJsonDecorator < BaseJsonDecorator
   def node
     node = hash_for(%w(id user_id commentable_id commentable_type parent_id body deleted))
-    node[:avatarLink] = model.user_id.zero? ? h.image_tag('guest_default_100.png') : model.user.decorate.avatar_link(:mini)
-    node[:userName] = model.user_id.zero? ? model.guest_name : model.user.id == -1 ? '<span>Deleted User</span>' : model.user.decorate.name_link
+    node[:avatarLink] = (model.user_id.zero? or model.user_id == -1) ? h.image_tag('guest_default_100.png') : model.user.decorate.avatar_link(:mini)
+    node[:userName] = model.user_id.zero? ? model.guest_name : model.user_id == -1 ? '<span>Deleted user</span>' : model.user.decorate.name_link
     node[:depth] = model.is_root? ? 0 : 1
     node[:createdAt] = model.created_at
     node
