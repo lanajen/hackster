3f418e982b037248748e0bd690b9253f4233026f
Benjamin Larralde <benjamin.larralde@gmail.com>
2015-12-10 23:38:22 -0800

    fix decorator for deleted user + styling

--- app/json_decorators/comment_json_decorator.rb
+++ app/json_decorators/comment_json_decorator.rb
@@ -2,8 +2,8 @@ class CommentJsonDecorator < BaseJsonDecorator
   def node
     node = hash_for(%w(id body user_id commentable_id commentable_type parent_id raw_body deleted))
     node[:avatarLink] = (model.user_id.zero? or model.user_id == -1) ? h.image_tag('guest_default_100.png') : model.user.decorate.avatar(:mini)
-    node[:userName] = model.user_id.zero? ? model.guest_name : model.user.id == -1 ? nil : model.user.name
-    node[:userSlug] = model.user_id.zero? ? nil : model.user.user_name
+    node[:userName] = model.user_id.zero? ? model.guest_name : (model.user_id == -1 ? nil : model.user.name)
+    node[:userSlug] = (model.user_id.zero? or model.user_id == -1) ? nil : model.user.user_name
     node[:depth] = model.is_root? ? 0 : 1
     node[:createdAt] = model.created_at
     node[:likingUsers] = model.liking_users.map(&:id) || []
