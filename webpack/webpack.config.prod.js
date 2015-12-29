b47e0624853e1c059a495ca753e0422f4fcf493c
Cory Asato <cory@hackster.io>
2015-12-29 12:04:50 -1000

    Done.

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -8,12 +8,6 @@ config.plugins = [
   new webpack.optimize.OccurenceOrderPlugin(),
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify('production')
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      screw_ie8: true,
-      warnings: false
-    }
   })
 ];
 
