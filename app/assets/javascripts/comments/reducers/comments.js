e56e35c8be5268cca8543f9e883bda7c808d2841
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-01-07 15:14:13 -0800

    fix comment like counter when unliking

--- app/assets/javascripts/comments/reducers/comments.js
+++ app/assets/javascripts/comments/reducers/comments.js
@@ -155,11 +155,13 @@ function toggleLikes(comments, commentId, parentId, userId, bool) {
 }
 
 function _addToOrRemoveFromArray(array, bool, item) {
+  console.log('array', array, 'bool', bool, 'item', item);
   if(bool) {
     array.push(item);
   } else {
-    array = array.filter((x) => { item !== x; });
+    array = array.filter((x) => { return item !== x; });
   }
+  console.log('array', array);
   return array;
 }
 
