592206e0bda2296c55376a3698a93d02e5993dcb
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-01-21 19:42:29 -0800

    cleanup

--- script/live_import.rb
+++ script/live_import.rb
@@ -1,7 +1,7 @@
 csv_text = "event_type,name,link,city,country,organizer_id,cover_image_url
 meetup,Hackster Live PHX,http://www.meetup.com/HacksterLivePHX,\"Phoenix, AZ\",United States,8388,http://photos1.meetupstatic.com/photos/event/c/7/a/b/600_445851115.jpeg
 meetup,Hackster Hardware Meetup - Seattle,http://www.meetup.com/Seattle-Arduino-Meetup,\"Seattle, WA\",United States,3475,https://c2.staticflickr.com/2/1552/24334006186_948d5fe58e_z.jpg
-meetup,Hackster.io Meetup -Bulawayo,http://www.meetup.com/Bulawayo-Social-Meetup-BSM,Bulawayo,Zimbabwe,28536,http://photos1.meetupstatic.com/photos/event/c/7/a/b/600_445851115.jpeg
+meetup,Hackster.io Meetup - Bulawayo,http://www.meetup.com/Bulawayo-Social-Meetup-BSM,Bulawayo,Zimbabwe,28536,http://photos1.meetupstatic.com/photos/event/c/7/a/b/600_445851115.jpeg
 meetup,Hackster Abuja,http://www.meetup.com/Hackster-Abuja/,Abuja,Nigeria,25045,http://photos1.meetupstatic.com/photos/event/c/7/a/b/600_445851115.jpeg
 meetup,Robot Garden,http://www.meetup.com/Robot-Garden/,\"Livermore, CA\",United States,43155,http://photos4.meetupstatic.com/photos/event/6/0/8/0/600_443664704.jpeg
 meetup,Makers Novi Sad,http://www.meetup.com/makers-ns,Novi Sad,Serbia,11436,http://photos4.meetupstatic.com/photos/event/5/5/7/7/600_439101879.jpeg
@@ -18,8 +18,9 @@ require 'csv'
 
 csv = CSV.parse(csv_text, headers: true)
 csv.each do |row|
-  live = LiveChapter.new name: row['name'], event_type: row['event_type'], link: row['link'], city: row['city'], country: row['country'], organizer_id: row['organizer_id']
+  live = LiveChapter.new full_name: row['name'], event_type: row['event_type'], home_link: row['link'], city: row['city'], country: row['country']
   live.build_cover_image
   live.cover_image.remote_file_url = row['cover_image_url']
+  live.members.new user_id: row['organizer_id'], group_roles: ['organizer']
   live.save
 end
