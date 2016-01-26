21021a45990a02ab18bd0c86121299b880c88daf
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-01-25 18:58:39 -0600

    course wizard (WIP)

--- app/assets/javascripts/course_wizard/actions.js
+++ app/assets/javascripts/course_wizard/actions.js
@@ -1,72 +1,43 @@
 import { fetchReviewThread, postDecision, postComment } from '../utils/ReactAPIUtils';
 
-export const REQUEST_THREAD = 'REQUEST_THREAD';
-export const RECEIVE_THREAD = 'RECEIVE_THREAD';
-export const SUBMIT_DECISION = 'SUBMIT_DECISION';
-export const SUBMIT_COMMENT = 'SUBMIT_COMMENT';
-export const SET_COMMENT_SUBMITTED = 'SET_COMMENT_SUBMITTED';
-export const SET_DECISION_SUBMITTED = 'SET_DECISION_SUBMITTED';
+export const SEARCH_UNIVERSITY = 'SEARCH_UNIVERSITY';
+export const RECEIVE_UNIVERSITIES = 'RECEIVE_UNIVERSITIES';
+export const SELECT_UNIVERSITY = 'SELECT_UNIVERSITY';
+export const SUBMIT_UNIVERSITY = 'SUBMIT_UNIVERSITY';
+export const RECEIVE_UNIVERSITY = 'RECEIVE_UNIVERSITY';
 
-function requestThread() {
+function searchUniversity(query) {
   return {
-    type: REQUEST_THREAD
+    type: SEARCH_UNIVERSITY,
+    query
   };
 }
 
-function submitComment(comment) {
+function receiveUniversities(results) {
   return {
-    type: SUBMIT_COMMENT,
-    comment
+    type: RECEIVE_UNIVERSITIES,
+    results
   };
 }
 
-function setCommentSubmitted(comment) {
+function selectUniversity(university) {
   return {
-    type: SET_COMMENT_SUBMITTED,
-    comment
+    type: SELECT_UNIVERSITY,
+    university
   };
 }
 
-function submitDecision(decision) {
+function submitUniversity(university) {
   return {
-    type: SUBMIT_DECISION,
-    decision
+    type: SUBMIT_UNIVERSITY,
+    university
   };
 }
 
-function setDecisionSubmitted(decision) {
+function receiveUniversity(university) {
   return {
-    type: SET_DECISION_SUBMITTED,
-    decision
-  };
-}
-
-function receiveThread(thread) {
-  return {
-    type: RECEIVE_THREAD,
-    thread
-  };
-}
-
-export function doSubmitComment(body, threadId, csrfToken) {
-  return function(dispatch) {
-
-    let comment = {
-      comment: {
-        raw_body: body
-      },
-      commentable: {
-        id: threadId,
-        type: 'review_thread'
-      }
-    };
-    dispatch(submitComment(comment));
-
-    let promise = postComment(comment, csrfToken);
-    return promise
-      .then(response =>
-        dispatch(setCommentSubmitted(response))
-      ).catch(function(err) { console.log('Request Error: ' + err); });
+    type: RECEIVE_UNIVERSITY,
+    university
   };
 }
 
@@ -79,12 +50,6 @@ export function doSubmitDecision(decision, projectId) {
       .then(response =>
         dispatch(setDecisionSubmitted(response.body.decision))
       ).catch(function(err) { console.log('Request Error: ' + err); });
-  };
-}
-
-export function initialFetch(projectId) {
-  return (dispatch, getState) => {
-    return dispatch(fetchThreadFromServer(projectId));
   };
 }
 
