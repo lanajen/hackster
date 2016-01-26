b0c0340e78aeb1add8851d2a8c6588740eba833e
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-01-26 12:38:07 -0800

    add review decision content to email notification

--- app/decorators/review_decision_decorator.rb
+++ app/decorators/review_decision_decorator.rb
@@ -1,4 +1,8 @@
 class ReviewDecisionDecorator < ApplicationDecorator
+  def decision
+    model.decision.humanize
+  end
+
   def rejection_reason
     ReviewDecision::REJECTION_REASONS[model.rejection_reason.try(:to_sym)]
   end
