34bb94f02b0fb122e5af7ce15859863a9f811712
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-02-01 19:45:09 -0800

    add email to ideas

--- app/workers/challenge_worker.rb
+++ app/workers/challenge_worker.rb
@@ -58,18 +58,17 @@ class ChallengeWorker < BaseWorker
 
     headers = ['Name', 'Description', 'Image URL']
     headers += challenge.challenge_idea_fields.map(&:label)
-    headers += ['Submitter name', 'Submission date', 'Submission status']
+    headers += ['Submitter name', 'Submission date', 'Submission status', 'Email']
     if challenge.pre_contest_awarded?
-      headers += ['Email', 'Full name', 'Address line 1', 'Address line 2', 'Zip code', 'State', 'Country', 'Phone number']
+      headers += ['Full name', 'Address line 1', 'Address line 2', 'Zip code', 'State', 'Country', 'Phone number']
     end
     rows = [headers]
     ideas.each do |idea|
       user = idea.user
       output = [idea.name, ActionController::Base.helpers.strip_tags(idea.description).gsub(/"/, '""'), idea.image.try(:imgix_url, :thumb)]
       output += challenge.challenge_idea_fields.each_with_index.map{|f, i| idea.send("cfield#{i}").try(:gsub, /"/, '""') }
-      output += [user.name, idea.created_at.in_time_zone(PDT_TIME_ZONE), idea.workflow_state]
+      output += [user.name, idea.created_at.in_time_zone(PDT_TIME_ZONE), idea.workflow_state, user.email]
       if challenge.pre_contest_awarded?
-        output += [user.email]
         if idea.won? and address = idea.address
           output += [address.full_name, address.address_line1, address.address_line2, address.zip, address.state, address.country, address.phone]
         else
