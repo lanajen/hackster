d5eb5e606db85175a095ee585d332ec3527805f3
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-03-07 11:27:25 -0800

    fix arduino expire script

--- script/expire_all_projects_from_arduino.rb
+++ script/expire_all_projects_from_arduino.rb
@@ -10,10 +10,9 @@ Cashier.expire *keys
 platform = Platform.find_by_user_name 'arduino'
 platform.project_collections.references(:project).includes(:project).visible.each do |col|
   p = col.project
-  next unless p.type == 'Project'
   keys = []
   p.respecting_users.each{|u| keys << "user-#{u.id}-thumb" }
-  p.replicated_users.each{|u| keys << "user-#{u.id}-thumb" }
+  p.replicated_users.each{|u| keys << "user-#{u.id}-thumb" } if p.respond_to? :replicated_users
   keys << "project-#{p.id}-respects"
   keys << "project-#{p.id}-replications"
   keys << "project-#{p.id}-thumb"
