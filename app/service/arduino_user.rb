6a050100f1a65c611171b2baffe88c14c66f6181
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-03-11 11:12:35 -0800

    logging in arduino_user

--- app/service/arduino_user.rb
+++ app/service/arduino_user.rb
@@ -8,15 +8,24 @@ class ArduinoUser
 
   def is_beta_tester?
     if user_info = get_user_info(@user_name, @token)
-      Rails.logger.debug "Arduino user_info: #{user_info.inspect}"
       parsed_info = JSON.parse user_info
-      parsed_info.try(:[], 'services').try(:[], 'create').try(:[], 'enabled') == true
+      Rails.logger.debug "Arduino user_info: #{parsed_info.inspect}"
+
+      if parsed_info.try(:[], 'services').try(:[], 'create').try(:[], 'enabled') == true
+        true
+      else
+        AppLogger.new("Arduino user `#{user_name}` appears unauthorized: #{parsed_info.inspect}",
+          'unauthorized',
+          'arduino').log
+        false
+      end
     end
   end
 
   private
     def get_user_info user_name, token
       Rails.logger.debug "Fetching Arduino user_info for `#{user_name}` (token: `#{token}`)"
+
       url = BASE_API_URL + user_name + '?app=create'
       token_header = "Token #{token}"
       resp = open(url,
@@ -27,11 +36,11 @@ class ArduinoUser
 
     rescue OpenURI::HTTPError => e
       # 500 error or something alike
-      AppLogger.new("Failed getting Arduino user_info for `#{user_name}`",
+      AppLogger.new("Failed to get Arduino user_info for `#{user_name}`",
         'http_error',
         'arduino',
         e).log.stdout
 
-      false
+      nil
     end
 end
