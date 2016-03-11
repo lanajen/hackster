ba1ec1c011d2aa1b4344f035af2925fb30f87645
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-03-11 10:12:42 -0800

    fix new project impressions for users/stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -19,7 +19,7 @@ class StatsController < ApplicationController
     @new_followers = @user.followers.where("follow_relations.created_at > ?", Date.today.beginning_of_month).count
     @new_views = @user.impressions.where("impressions.created_at > ?", Date.today.beginning_of_month).count
     @new_respects = Respect.where(respectable_type: 'BaseArticle', respectable_id: @user.projects.own.pluck(:id)).where("respects.created_at > ?", Date.today.beginning_of_month).count
-    @new_project_views = Impression.where(impressionable_type: 'BaseArticle', impressionable_id: @user.projects.own.pluck(:id)).where("impressions.created_at > ?", Date.today.beginning_of_month).count
+    @new_project_views = ProjectImpression.where(project_id: @user.projects.own.pluck(:id)).where("project_impressions.created_at > ?", Date.today.beginning_of_month).count
 
     @reputation_events = @user.reputation_events.order(event_date: :desc) if current_user.is? :admin
   end
