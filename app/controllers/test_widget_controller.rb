2cfeb7a9dd16f26b4b9d1b58342693700ebfdf11
ZACK DEAN <vanleuvenze@gmail.com>
2016-03-25 18:12:13 -0700

    exposing the embed_widgets component through this slim template

--- app/controllers/test_widget_controller.rb
+++ app/controllers/test_widget_controller.rb
@@ -1,8 +1,10 @@
 class TestWidgetController < ApplicationController
 
   def index
-    @username = current_user.inspect
+    @username = current_user.user_name
     @project_count = current_user.projects_count
+
   end
 
 end
+
