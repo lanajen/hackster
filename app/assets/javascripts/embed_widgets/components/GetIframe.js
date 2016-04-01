9a1d9f905133e2333d913d7294b9189dea03b117
ZACK DEAN <vanleuvenze@gmail.com>
2016-03-31 17:48:11 -0700

    refactored the Embed components, all state is now held in the App component

--- app/assets/javascripts/embed_widgets/components/GetIframe.js
+++ app/assets/javascripts/embed_widgets/components/GetIframe.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 
 let styles = {
-  'width' : '100%',
-  'height': '100px',
-  'backgroundColor': 'blue'
-}
+  'width': '100%',
+  'height': '100%',
+  'display': 'block',
+  'marginBottom': '10px'
+};
 
 let inputStyles = {
-  'width' : '300px'
-}
-
-
+  'width' : '100%',
+  'marginBottom': '10px'
+};
 
 const GetIframe = ({
-  userId
+  iframeURL
 }) => {
-  let url = 'http://www.localhost.local:5000/'+ userId + '/embed';
 
   return (
     <div style={styles}>
       <input style={inputStyles} type="text"
-        value={"<iframe frameBorder='0' height='270' scrolling='no' src=" + url + " width='360'></iframe>"}
+        value={"<iframe frameBorder='0' height='270' scrolling='no' src=" + iframeURL + " width='360'></iframe>"}
       />
+      <iframe frameBorder='0' height='270' scrolling='no' src={iframeURL} width='360'></iframe>
     </div>
   )
 }
