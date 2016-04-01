8909b2cefa2a7082b242dad0359d19f016a63633
ZACK DEAN <vanleuvenze@gmail.com>
2016-03-31 17:49:32 -0700

    renamed embed_widget to embed_widgets

--- app/controllers/embed_widgets_controller.rb
+++ app/controllers/embed_widgets_controller.rb
@@ -1,6 +1,7 @@
-class EmbedWidgetController < ApplicationController
+class EmbedWidgetsController < ApplicationController
 
   def index
+    @user = current_user
     @username = current_user.user_name
     @project_count = current_user.projects_count
     @followers = current_user.followers_count
