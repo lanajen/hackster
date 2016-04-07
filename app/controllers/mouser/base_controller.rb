716c335efa0908d60347b3d56234aca114fad5aa
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-04-06 17:09:44 -0700

    initial routes for mouser contest

---
+++ app/controllers/mouser/base_controller.rb
@@ -0,0 +1,26 @@
+class Mouser::BaseController < ActionController::Base
+layout 'mouser'
+helper_method :title
+helper_method :meta_desc
+
+private
+  def meta_desc meta_desc=nil
+    if meta_desc
+      @meta_desc = meta_desc
+    elsif @meta_desc
+      @meta_desc
+    else
+      @meta_desc = 'Mouser Summer Contest'
+    end
+  end
+
+  def title title=nil
+    if title
+      @title = title
+    elsif @title
+      @title
+    else
+      @title = 'Mouser Summer Contest'
+    end
+  end
+end
