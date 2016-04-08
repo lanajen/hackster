f431b77c955abbbd2cc67ab228914f823810b71c
ZACK DEAN <vanleuvenze@gmail.com>
2016-04-08 12:25:09 -0700

    refactoring project import flow into redux

--- app/assets/javascripts/mouser_contest/components/authenticateComponent.js
+++ app/assets/javascripts/mouser_contest/components/authenticateComponent.js
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
 
 
-export default function authenticateComponent (Component) {
+// export default function authenticateComponent (Component) {
 
 
-  class AuthenticatedComponent extends Component {
+//   class AuthenticatedComponent extends Component {
 
-    componentWillMount() {
-      this.checkAuth()
-    }
+//     componentWillMount() {
+//       this.checkAuth()
+//     }
 
-    componentWillReceiveProps() {
-      this.checkAuth();
-    }
+//     componentWillReceiveProps() {
+//       this.checkAuth();
+//     }
 
-    checkAuth() {
-      if (!this.props.isAuthenticated) {
-        console.log('THIS USER IS NOT AUTHENTICATED');
-      }
-    }
+//     checkAuth() {
+//       if (!this.props.isAuthenticated) {
+//         console.log('THIS USER IS NOT AUTHENTICATED');
+//       }
+//     }
 
-    render() {
-      return (
-        <div>
-          {this.props.isAuthenticated === true
-            ? <Component/>
-            : null
-          }
-        </div>
-      )
-    }
-  }
+//     render() {
+//       return (
+//         <div>
+//           {this.props.isAuthenticated === true
+//             ? <Component/>
+//             : null
+//           }
+//         </div>
+//       )
+//     }
+//   }
 
-  const mapStateToProps = (state) => ({
-    token: state.auth.token,
-    isAuthenticated: state.auth.isAuthenticated
-  });
+//   const mapStateToProps = (state) => ({
+//     token: state.auth.token,
+//     isAuthenticated: state.auth.isAuthenticated
+//   });
 
-  return connect(mapStateToProps)(AuthenticatedComponent);
+//   return connect(mapStateToProps)(AuthenticatedComponent);
 
-}
+// }
