0de977a8a9e5b564f3542ea00be3121835a9bea4
ZACK DEAN <vanleuvenze@gmail.com>
2016-04-08 18:05:57 -0700

    basic functionality of importing project complete - setting up testing

--- db/migrate/20160408230901_add_fields_to_mouser_contest.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToMouserContest < ActiveRecord::Migration
-  def change
-    remove_column :mouser_submissions, :status, :integer
-    add_column :mouser_submissions, :status, :string
-  end
-end
