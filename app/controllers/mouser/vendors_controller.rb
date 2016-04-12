7e0feef9760f050ecba2dde09541e1df40b54b27
Cory Asato <cory@hackster.io>
2016-04-12 10:40:00 -0700

    Working on splash page timeline

--- app/controllers/mouser/vendors_controller.rb
+++ app/controllers/mouser/vendors_controller.rb
@@ -3,6 +3,16 @@ class Mouser::VendorsController < Mouser::BaseController
 
   def index
     @platforms = Platform.where(user_name: PLATFORM_USER_NAMES)
+    @boards = [ 'Cypress', 'Diligent', 'Intel', 'NXP', 'Seeed Studio', 'ST Micro', 'TI', 'UDOO' ]
+    @dates = [
+      {date: 'May 1st', action: 'Project submissions open'},
+      {date: 'May 15th', action: 'Project submissions close', subAction: 'Prelimenary voting begins'},
+      {date: 'May 31st', action: 'Project voting ends', subAction: 'Vendor champions chosen'},
+      {date: 'June 1st', action: 'Round 1 begins'},
+      {date: 'June 10th', action: 'Round 2 begins'},
+      {date: 'June 20th', action: 'Round 3 begins'},
+      {date: 'June 30th', action: 'Competition ends', subAction: 'Winner announced'}
+    ]
   end
 
   def show
