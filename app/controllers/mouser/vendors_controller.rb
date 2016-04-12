7840fa2e1c53a4d5c7f14b5935da3f1f2ca7e96d
Cory Asato <cory@hackster.io>
2016-04-12 15:59:17 -0700

    Finishes Vendors/Splash & Footer

--- app/controllers/mouser/vendors_controller.rb
+++ app/controllers/mouser/vendors_controller.rb
@@ -5,13 +5,13 @@ class Mouser::VendorsController < Mouser::BaseController
     @platforms = Platform.where(user_name: PLATFORM_USER_NAMES)
     @boards = [ 'Cypress', 'Diligent', 'Intel', 'NXP', 'Seeed Studio', 'ST Micro', 'TI', 'UDOO' ]
     @dates = [
-      {date: 'May 1st', action: 'Project submissions open'},
-      {date: 'May 15th', action: 'Project submissions close', subAction: 'Prelimenary voting begins'},
-      {date: 'May 31st', action: 'Project voting ends', subAction: 'Vendor champions chosen'},
-      {date: 'June 1st', action: 'Round 1 begins'},
-      {date: 'June 10th', action: 'Round 2 begins'},
-      {date: 'June 20th', action: 'Round 3 begins'},
-      {date: 'June 30th', action: 'Competition ends', subAction: 'Winner announced'}
+      {date: 'May 1st', event: 'Project submissions open'},
+      {date: 'May 15th', event: 'Project submissions close', sub_action: 'Prelimenary voting begins'},
+      {date: 'May 31st', event: 'Project voting ends', sub_action: 'Vendor champions chosen'},
+      {date: 'June 1st', event: 'Round 1 begins'},
+      {date: 'June 10th', event: 'Round 2 begins'},
+      {date: 'June 20th', event: 'Round 3 begins'},
+      {date: 'June 30th', event: 'Competition ends', sub_action: 'Winner announced'}
     ]
   end
 
