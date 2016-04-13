f73742d2503b4d683b46f2eac1cc35b4e2f68e19
Cory Asato <cory@hackster.io>
2016-04-13 12:55:40 -0700

    Adds @dates back to mouser controller for view to work properly.

--- app/controllers/mouser/vendors_controller.rb
+++ app/controllers/mouser/vendors_controller.rb
@@ -4,6 +4,15 @@ class Mouser::VendorsController < Mouser::BaseController
   def index
     @platforms = Platform.where(user_name: PLATFORM_USER_NAMES)
     @boards = [ 'Cypress', 'Digilent', 'Intel', 'NXP', 'Seeed Studio', 'ST Micro', 'Texas Instruments', 'UDOO' ] #Remove when done testing.
+    @dates = [
+      {date: 'May 1st', event: 'Project submissions open'},
+      {date: 'May 15th', event: 'Project submissions close', sub_action: 'Prelimenary voting begins'},
+      {date: 'May 31st', event: 'Project voting ends', sub_action: 'Vendor champions chosen'},
+      {date: 'June 1st', event: 'Round 1 begins'},
+      {date: 'June 10th', event: 'Round 2 begins'},
+      {date: 'June 20th', event: 'Round 3 begins'},
+      {date: 'June 30th', event: 'Competition ends', sub_action: 'Winner announced'}
+    ];
   end
 
   def show
