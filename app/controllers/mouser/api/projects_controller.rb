d7f466aabc7fa947e679b9ec6cd489186fb66000
Zack Dean <vanleuvenze@gmail.com>
2016-04-20 15:22:14 -0700

    Update projects_controller.rb
    Small tweak.  Last one maybe?

--- app/controllers/mouser/api/projects_controller.rb
+++ app/controllers/mouser/api/projects_controller.rb
@@ -1,7 +1,6 @@
 class Mouser::Api::ProjectsController < Mouser::Api::BaseController
   def index
     projects = BaseArticle.publyc.own.joins(:users).where(users: { id: params[:user_id] })
-    # render json: BaseCollectionJsonDecorator.new(projects).node
-    render json: projects
+    render json: BaseCollectionJsonDecorator.new(projects).node
   end
 end
