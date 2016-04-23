c61cd85035149e30932f8edb8c4397571005dec4
Cory Asato <cory@hackster.io>
2016-04-22 18:03:56 -0700

    Remove hard set param in controller.

--- app/controllers/mouser/api/projects_controller.rb
+++ app/controllers/mouser/api/projects_controller.rb
@@ -1,7 +1,7 @@
 class Mouser::Api::ProjectsController < Mouser::Api::BaseController
   def index
     projects = BaseArticle.publyc.own.where(type: 'Project').joins(:users).where(users: { id: params[:user_id] })
-    submissions = MouserSubmission.where(user_id: 8242) #Change to params[:user_id]
+    submissions = MouserSubmission.where(user_id: params[:user_id])
 
     render json: { projects: ProjectCollectionJsonDecorator.new(projects).node, submissions: submissions }
   end
