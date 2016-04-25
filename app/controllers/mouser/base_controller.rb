1e9e35d58182fc7a63318bd78b9eaf22f67931cf
Cory Asato <cory@hackster.io>
2016-04-25 11:18:50 -0700

    Move integer conversion on active_phase to controller.

--- app/controllers/mouser/base_controller.rb
+++ app/controllers/mouser/base_controller.rb
@@ -10,7 +10,7 @@ class Mouser::BaseController < ActionController::Base
 
   private
     def active_phase
-      @active_phase ||= redis.get('active_phase') || -1  # Set 'mouser:active_phase' to 0 in your redis db.
+      @active_phase ||= redis.exists('active_phase') ? redis.get('active_phase').to_i : -1  # Set 'mouser:active_phase' to 0 in your redis db.
     end
 
     def config
