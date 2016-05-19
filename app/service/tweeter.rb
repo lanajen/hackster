fa35db6d3ca2f14deae6d31b4633d7fff89906f9
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-05-19 16:48:47 -0700

    fix tweeter

--- app/service/tweeter.rb
+++ app/service/tweeter.rb
@@ -31,4 +31,14 @@ class Tweeter
     #   LogLine.create(log_type: 'error', source: 'twitter', message: "Error: #{e.inspect} // Tweet: \"#{message}\"")
     end
   end
+
+  private
+    def twitter_client
+      @twitter_client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key        = TWITTER_CONSUMER_KEY
+        config.consumer_secret     = TWITTER_CONSUMER_SECRET
+        config.access_token        = TWITTER_ACCESS_TOKEN
+        config.access_token_secret = TWITTER_ACCESS_SECRET
+      end
+    end
 end
--- app/workers/twitter_queue.rb
+++ app/workers/twitter_queue.rb
@@ -40,13 +40,4 @@ class TwitterQueue < BaseWorker
     def redis
       @redis ||= Redis::Namespace.new('twitter_queue', redis: RedisConn.conn)
     end
-
-    def twitter_client
-      @twitter_client ||= Twitter::REST::Client.new do |config|
-        config.consumer_key        = TWITTER_CONSUMER_KEY
-        config.consumer_secret     = TWITTER_CONSUMER_SECRET
-        config.access_token        = TWITTER_ACCESS_TOKEN
-        config.access_token_secret = TWITTER_ACCESS_SECRET
-      end
-    end
 end
