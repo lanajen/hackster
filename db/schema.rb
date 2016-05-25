612b129bf673b5831612d6db075735ef98e8e99c
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-05-25 10:30:51 -0700

    arduino tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160512180341) do
-=======
-ActiveRecord::Schema.define(version: 20160518223953) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
