88203425f5f3be1ad515c6f9617aabf1e5563452
Benjamin Larralde <benjamin.larralde@gmail.com>
2016-05-30 11:45:59 -0700

    fix username generation

--- app/service/user_name_generator.rb
+++ app/service/user_name_generator.rb
@@ -8,29 +8,31 @@ class UserNameGenerator
   end
 
   private
+    def base_user_name base=nil
+      if _name = format_as_user_name(base) and _name.present?
+        return _name, 1..3
+      else
+        return BASE_USER_NAME, 5..10
+      end
+    end
+
     def format_as_user_name text
+      return unless text.present?
+
       I18n.transliterate(text).gsub(/[^a-zA-Z0-9\-_]/, '-').gsub(/(\-)+$/, '').gsub(/^(\-)+/, '').gsub(/(\-){2,}/, '-').downcase
     end
 
     def generate_user_name base=nil
-      if base.present?
-        user_name = format_as_user_name base
-        while SlugHistory.where(value: user_name).exists?
-          user_name = format_as_user_name(base) + rand(1..2).times.map{rand(9) }.join('')
-        end
-        user_name
-      else
-        user_name_with_numbers = get_number_version(BASE_USER_NAME)
-        while SlugHistory.where(value: user_name_with_numbers).exists?
-          user_name_with_numbers = get_number_version(BASE_USER_NAME)
-        end
-
-        user_name_with_numbers
+      _base, nums = base_user_name(base)
+      _name = _base.dup
+      while SlugHistory.where(value: _name).exists?
+        _name = get_number_version(_base, nums)
       end
+      _name
     end
 
-    def get_number_version base_user_name
-      numbers = rand(5..10).times.map{ rand(9) }.join('')
-      "#{base_user_name}#{numbers}"
+    def get_number_version base, nums
+      numbers = rand(nums).times.map{ rand(9) }.join('')
+      "#{base}#{numbers}"
     end
 end
